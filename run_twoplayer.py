9ec7768652797bd42ffe3749f6c4b78a0c3448c7
Mikhail Terekhov <mkl.terekhov@gmail.com>
2023-09-30 17:35:07 +0200

    Add the simplest two-player command-line setup

---
+++ run_twoplayer.py
@@ -0,0 +1,38 @@
+import re
+import argparse
+import json
+from pathlib import Path
+import gradio as gr
+from glob import glob
+import tqdm
+from datetime import datetime
+
+from chatarena.arena import Arena, TooManyInvalidActions
+from chatarena.backends import BACKEND_REGISTRY
+from chatarena.backends.human import HumanBackendError
+from chatarena.config import ArenaConfig
+from chatarena.environments import ENV_REGISTRY
+from chatarena.database import log_arena, log_messages, SupabaseDB, supabase_available
+from chatarena.message import Message
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("config", type=Path)
+parser.add_argument("--num_steps", type=int, default=20)
+args = parser.parse_args()
+
+out_dir = Path(f"output/{datetime.now().strftime('%Y%m%d_%H%M%S')}")
+Path("output").mkdir(exist_ok=True)
+out_dir.mkdir(exist_ok=True)
+
+conf = json.load(args.config.open())
+arena_conf = ArenaConfig(**conf)
+print(conf)
+arena = Arena.from_config(arena_conf)
+
+for _ in tqdm.trange(args.num_steps, desc="Conversation"):
+    arena.step()
+
+arena.save_history(str(out_dir / "history.json"))
+
+
