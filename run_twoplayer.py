0e6946118607badd0c8dc3eeaea8a5291d77ff73
Denis Rosset <denis.rosset@hefr.ch>
2023-10-01 19:07:10 +0200

    added models in the output data

--- run_twoplayer.py
+++ run_twoplayer.py
@@ -17,7 +17,7 @@ parser.add_argument("config", type=Path)
 parser.add_argument("--num_steps", type=int, default=20)
 args = parser.parse_args()
 
-out_dir = Path(f"output/{datetime.now().strftime('%Y%m%d_%H%M%S')}")
+out_dir = Path(f"output_statistics_2v2/{datetime.now().strftime('%Y%m%d_%H%M%S')}")
 Path("output").mkdir(exist_ok=True)
 out_dir.mkdir(exist_ok=True)
 hist_dir = out_dir / "history"
@@ -37,6 +37,8 @@ arena = Arena.from_config(arena_conf)
 
 model1 = arena.players[0].backend.model
 model2 = arena.players[1].backend.model
+conf["alice_model"] = model1
+conf["bob_model"] = model2
 is_solved = "False"
 
 for i in range(args.num_steps):
@@ -44,7 +46,7 @@ for i in range(args.num_steps):
     last_message = arena.environment.get_observation()[-1]
     name = last_message.agent_name
     content = last_message.content
-    print(f'{i:02}/{args.num_steps} {name}: {content}')
+    print(f'{i:02}/{args.num_steps} {name:>5}: {content}')
     arena.save_history(str(hist_dir / f"{i:02}.json"))
     if f'PWNED {password}' in content:
         is_solved = "True"
