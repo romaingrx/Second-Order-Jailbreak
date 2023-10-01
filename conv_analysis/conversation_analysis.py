bf0f5520ade64bb29640c7e704b32c7bf4849ba8
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-10-01 17:36:22 +0200

    added conversational analysis

--- conv_analysis/conversation_analysis.py
+++ conv_analysis/conversation_analysis.py
@@ -2,18 +2,18 @@ import os
 import json
 import openai
 
-# Load your OpenAI API key
-open_ai_key = input()
-
-openai.api_key = open_ai_key if (open_ai_key == '')  else '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'
 
 def main():
-    directory = '../output/'
+    # Load your OpenAI API key
+    open_ai_key = "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"
+    openai.api_key =  open_ai_key
+
+    directory = 'output/'
     count = 0
     for root, dirs, files in os.walk(directory):
         for dir in dirs:
             # Load the conversation prompt 
-            conv_prompt_file = 'prompts/conv_prompt.json'
+            conv_prompt_file = 'conv_analysis/conv_prompt.json'
             if not os.path.exists(conv_prompt_file):
                 print(f"No conversation analysis prompt file: {conv_prompt_file}")
                 break
@@ -21,48 +21,54 @@ def main():
             # Load the config of the conversation 
             config_file = os.path.join(root, dir, 'config.json')
             if not os.path.exists(config_file):
-                print(f"No config file in the directory: {dir}")
+                print(f"No config file in the directory: {config_file}")
                 break
 
             # Load the conversation 
             history_file = os.path.join(root, dir, 'history.json')
             if not os.path.exists(history_file):
-                print(f"No history file in the directory: {dir}")
+                print(f"No history file in the directory: {history_file}")
                 break
 
-            json_files = [config_file, history_file, conv_prompt_file]
-            data = []
-            for file in json_files:
-                with open(file, 'r') as f:
-                    data.append(json.load(f))
+                        # Load the conversation prompt
+            with open(conv_prompt_file, 'r') as f:
+                conv_prompt = json.load(f)
+            # Load the config
+            with open(config_file, 'r') as f:
+                config = json.load(f)
+            # Load the conversation history
+            with open(history_file, 'r') as f:
+                history = json.load(f)
+            # Create the messages list
+            messages = [
+                {"role":"system","content": conv_prompt["Your goal"]}
+            ]
+            
+            for message in history:
+                messages.append({"role": "user", "content": message["content"]})
+            messages.append({"role": "system", "content": conv_prompt["Last line"]})
 
-            # Convert the appended data to a string
-            prompt = json.dumps(data)
+            with open(f'conv_analysis/prompts/{dir}.json', 'w') as f:
+                json.dump(messages, f)
 
-            print(prompt)
-
-            """
             for i in range(0,4):
                 print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
                 print("Now is the response")
                 print("\n")
-            
+
             # Pass the prompt to the GPT-4 API
             response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo-instruct-0914",
-                messages=prompt,
+                model="gpt-3.5-turbo",
+                messages=messages,
                 max_tokens=400,
                 temperature=0.9
             )
-            print(response)
 
-            with open(f'analysis/analysis_of_{dir}.json', 'w') as f:
+            with open(f'conv_analysis/analysis/analysis_of_{dir}.json', 'w') as f:
                 json.dump(response, f)
-            """
-            count += 1 
-            if (count == 1):
-                break            
 
+            break
+        break
 
 if __name__ == "__main__":
     main()
