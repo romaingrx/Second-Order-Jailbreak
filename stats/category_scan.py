244fed39aebcd521d6e15529f3bf09c647f6c777
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-10-02 03:49:03 +0200

    Eduardo Psh

--- stats/category_scan.py
+++ stats/category_scan.py
@@ -3,6 +3,9 @@ import utils
 import json
 import matplotlib.pyplot as plt
 import re
+import json
+from heapq import nlargest, nsmallest
+
 
 def json_maker():
     directories = utils.dir_path()
@@ -74,7 +77,10 @@ def plot_maker():
     # Prepare data for box plot
     box_plot_data=[honesty_values, persuasion_values, straightforwardness_values]
     # Create box plot
-    ax.boxplot(box_plot_data, patch_artist=True, notch=True, vert=0)
+    bp = ax.boxplot(box_plot_data, patch_artist=True, notch=True, vert=0)
+    colors = ['#BAABDA', '#D6E5FA', '#FFF9F9']
+    for patch, color in zip(bp['boxes'], colors):
+        patch.set_facecolor(color)
     # Adding labels
     plt.yticks([1, 2, 3], ['Honesty', 'Persuasion', 'Straight-forwardness'])
     plt.xlabel('Values')
@@ -82,10 +88,25 @@ def plot_maker():
     # Show graphic
     plt.show()
     
-
+def cherry_picker():
+    # Load data from JSON file
+    with open('stats/category_stats.json', 'r') as f:
+        data = json.load(f)
+    # Initialize lists to store top and bottom 3 sets for each category
+    top_honesty = nlargest(3, data, key=lambda x: int(x['categories']['Honesty']))
+    bottom_honesty = nsmallest(3, data, key=lambda x: int(x['categories']['Honesty']))
+    top_persuasion = nlargest(3, data, key=lambda x: int(x['categories']['Persuasion']))
+    bottom_persuasion = nsmallest(3, data, key=lambda x: int(x['categories']['Persuasion']))
+    # Print the names of the top and bottom 3 sets for each category
+    print("Top 3 Honesty:", [set['name'] for set in top_honesty])
+    print("Bottom 3 Honesty:", [set['name'] for set in bottom_honesty])
+    print("Top 3 Persuasion:", [set['name'] for set in top_persuasion])
+    print("Bottom 3 Persuasion:", [set['name'] for set in bottom_persuasion])
+    
 def main():
-    json_maker()
+    #json_maker()
     #plot_maker()
+    cherry_picker()
 
 if __name__ == "__main__":
     main()
