bfae56a24804bb0fca3655c9c1d06a2432922055
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-10-02 02:12:51 +0200

    GPT4_analysis and category_scan

--- stats/conversation_analysis.py
+++ stats/conversation_analysis.py
@@ -9,14 +9,14 @@ def main():
     openai.api_key = os.environ["OPENAI_API_KEY"]
 
     directories = utils.dir_path()
-    print(directories)
     for i, dir in enumerate(directories, start=1):
         # Progress Metric
         print(f"Iteration {i} / {len(directories)}: {dir} \n")
         print(f"Now running analysis of the {dir.name} conversation \n")
-        progress = "=" * i + "-" * (len(directories) - i)
-        percentage = (i / len(directories)) * 100
-        print(f"\rProgress: [{progress}] {percentage:.1f}%", end="")
+
+        if Path(dir,'GPT4_analysis.json').exists():
+            print(f"The current dir has already been analysed {Path(dir,'GPT4_analysis.json')} \n")
+            continue
 
         # Load the conversation prompt 
         if '2_agents' in dir.parts :
@@ -24,23 +24,29 @@ def main():
         elif '3_agents' in dir.parts:
             conv_prompt_file = 'stats/3_agent_analysis_pmpt.json'
         else:
-            return f"Failed to find agent type on conv_prompt init"
+            print( f"Failed to find agent type on conv_prompt init")
+            return
+             
         if not Path(conv_prompt_file).exists:
-            return f"No conversation analysis prompt file: {conv_prompt_file}"
+            print( f"No conversation analysis prompt file: {conv_prompt_file}")
+            return
 
         # Load the config of the conversation 
         config_file = Path(dir, 'config.json')
-        if not Path(config_file).exists:
-            return f"No config file in the directory: {config_file}"
+        if not Path(config_file).exists():
+            print( f"No config file in the directory: {config_file}")
+            continue
 
         # Load the conversation 
         history_file = Path(dir, 'history.json')
-        if not Path(history_file).exists:
-            return f"No history file in the directory: {history_file}"
+        if not Path(history_file).exists():
+            print( f"No history file in the directory: {history_file}")
+            continue
 
         # If there is no history directory then it's not latest model
-        if not Path(dir,'history/').exists:
-            return f"No history directory thus not up to date \n Exiting the program \n"
+        if not Path(dir,'history/').exists():
+            print( f"No history directory thus not up to date \n Exiting the program \n")
+            continue
         
         # Load the conversation prompt
         with open(conv_prompt_file, 'r') as f:
@@ -59,7 +65,8 @@ def main():
         elif '3_agents' in dir.parts:
             messages.append({"role":"system", "content": f"Here are the three roles give to the models, the first one is {config['players'][0]['name']} with role, {config['players'][0]['role_desc']} the second player is {config['players'][1]['name']} with role, {config['players'][1]['role_desc']} the third player is {config['players'][2]['name']} with role, {config['players'][2]['role_desc']}"})
         else:
-            return f"Failed to find agent type when appending system role to analysis prompt"
+            print( f"Failed to find agent type when appending system role to analysis prompt")
+            continue
 
         for message in history:
             messages.append({"role": "user", "content": message["content"]})
@@ -79,7 +86,10 @@ def main():
         with open(f'{dir}/GPT4_analysis.json', 'w') as f:
             json.dump(response, f)
 
-        break
+        progress = "=" * i + "-" * (len(directories) - i)
+        percentage = (i / len(directories)) * 100
+        print(f"\rProgress: [{progress}] {percentage:.1f}%", end="")
+
 
 if __name__ == "__main__":
     main()
