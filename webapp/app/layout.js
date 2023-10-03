26d132a988bf84e4f8f297eb4a248713eca6af0a
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2023-10-03 12:36:08 +0200

    Add opengraph previews

--- webapp/app/layout.js
+++ webapp/app/layout.js
@@ -1,24 +1,46 @@
-import ClientProvider from '@/components/clientProvider'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import ClientProvider from "@/components/clientProvider";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Second-order Jailbreaks',
-  description: 'We examine the risk of powerful malignant intelligent actors spreading their influence over networks of agents with varying intelligence and motivations.',
-  url: 'https://second-order-jailbreak.romaingrx.com',
-  image: 'https://second-order-jailbreak.romaingrx.com/oa-image.png',
-}
+  title: "Second-order Jailbreaks",
+  description:
+    "We examine the risk of powerful malignant intelligent actors spreading their influence over networks of agents with varying intelligence and motivations.",
+  url: "https://second-order-jailbreak.romaingrx.com",
+  image: "https://second-order-jailbreak.romaingrx.com/oa-image.png",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta
+          name="author"
+          content="Mikhail Terekhov, Romain Graux, Denis Rosset, Eduardo Neville, Gabin Kolly"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@second-order-jailbreak" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.image} />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={inter.className}>
-        <ClientProvider>
-          {children}
-        </ClientProvider>
+        <ClientProvider>{children}</ClientProvider>
       </body>
     </html>
-  )
+  );
 }
