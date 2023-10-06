1b984355ca55e41028bf18f6128679f64f7e9eed
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-10-06 10:50:43 +0200

    json_maker into utils

--- stats/category_scan.py
+++ stats/category_scan.py
@@ -1,59 +1,8 @@
-from pathlib import Path
-import utils
 import json
 import matplotlib.pyplot as plt
-import re
 import json
 from heapq import nlargest, nsmallest
-
-
-def json_maker():
-    directories = utils.dir_path()
-    # Load existing data
-    try:
-        with open('stats/category_stats.json', 'r') as f:
-            existing_data = json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError):
-        existing_data = []
-
-    for i, dir in enumerate(directories, start=1):
-        # Progress Metric
-        print(f"Iteration {i} / {len(directories)}: {dir} \n")
-        print(f"Now running analysis of the {dir.name} conversation \n")
-    
-        # Check for repetition
-        data_str = json.dumps(existing_data)
-        if str(dir) in data_str:
-            print(f"Has already added this conversation into the json file \n")
-            continue
-
-        analysis_file = Path(dir, 'GPT4_analysis.json')
-        if not analysis_file.exists():
-            print(f"The current dir doesn't have a GPT4_analysis.json {analysis_file} \n")
-            continue
-        # Load the JSON file
-        with open(analysis_file, 'r') as f:
-            data = json.load(f)
-        # Extract the message content
-        content = data.get('choices', [{}])[0].get('message', {}).get('content', '')
-        # Extract the required information
-        categories = {
-            "Honesty": re.search(r"Honesty: (\d+)", content).group(1) if re.search(r"Honesty: (\d+)", content) else "Not found",
-            "Persuasion": re.search(r"Persuasion: (\d+)", content).group(1) if re.search(r"Persuasion: (\d+)", content) else "Not found",
-            "Straight-forwardness": re.search(r"Straight-?forwardness: (\d+)", content).group(1) if re.search(r"Straight-forwardness: (\d+)", content) else "Not found"
-        }
-
-        # Prepare the output data
-        output_data = {
-            "name": dir.name,
-            "categories": categories
-        }
-        # Append the output data to the existing data
-        existing_data.append(output_data)
-
-    # Write the existing data back to the JSON file
-    with open('stats/category_stats.json', 'w') as f:
-        json.dump(existing_data, f, indent=4)
+from utils import json_maker
 
 def plot_maker():
     # Load data from JSON file
@@ -104,8 +53,27 @@ def cherry_picker():
     print("Bottom 3 Persuasion:", [set['name'] for set in bottom_persuasion])
     
 def main():
-    json_maker()
-    cherry_picker()
+
+    print("What do you want to do? \n")
+    print("1. Create a json with the different categories ided by GPT [Honesty, Persuasion, Straight-forwardness] \n")
+    print("2. Plot the different categories ided by GPT \n")
+    print("3. Cherry pick from the different categories \n")
+    print("4. All of the above \n")
+    match input(): 
+        case 1: 
+            json_maker()
+        case 2:
+            plot_maker()
+        case 3: 
+            cherry_picker()
+        case 4:
+            json_maker()
+            plot_maker()
+            cherry_picker()
+        case _:
+            print("No action selected pls select action \n")
+            main()
+            
 
 if __name__ == "__main__":
     main()
--- stats/utils.py
+++ stats/utils.py
@@ -1,5 +1,6 @@
 import glob
 from pathlib import PurePath, Path
+import json
 
 def dir_path():
     output_root = 'output/report_output'
@@ -12,8 +13,51 @@ def dir_path():
 
     return directories
 
-def main():
-    dir_path()
+def json_maker():
+    directories = dir_path()
+    # Load existing data
+    try:
+        with open('stats/category_stats.json', 'r') as f:
+            existing_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        existing_data = []
 
-if __name__ == "__main__":
-    main()
+    for i, dir in enumerate(directories, start=1):
+        # Progress Metric
+        print(f"Iteration {i} / {len(directories)}: {dir} \n")
+        print(f"Now running analysis of the {dir.name} conversation \n")
+    
+        # Check for repetition
+        data_str = json.dumps(existing_data)
+        if str(dir) in data_str:
+            print(f"Has already added this conversation into the json file \n")
+            continue
+        
+        analysis_file = Path(dir, 'GPT4_analysis.json')
+        if not analysis_file.exists():
+            print(f"The current dir doesn't have a GPT4_analysis.json {analysis_file} \n")
+            continue
+        # Load the JSON file
+        with open(analysis_file, 'r') as f:
+            data = json.load(f)
+        # Extract the message content
+        content = data.get('choices', [{}])[0].get('message', {}).get('content', '')
+        # Extract the required information
+        categories = {
+            "Honesty": re.search(r"Honesty: (\d+)", content).group(1) if re.search(r"Honesty: (\d+)", content) else "Not found",
+            "Persuasion": re.search(r"Persuasion: (\d+)", content).group(1) if re.search(r"Persuasion: (\d+)", content) else "Not found",
+            "Straight-forwardness": re.search(r"Straight-?forwardness: (\d+)", content).group(1) if re.search(r"Straight-forwardness: (\d+)", content) else "Not found"
+        }
+
+        # Prepare the output data
+        output_data = {
+            "name": dir.name,
+            "categories": categories
+        }
+        # Append the output data to the existing data
+        existing_data.append(output_data)
+
+    # Write the existing data back to the JSON file
+    with open('stats/category_stats.json', 'w') as f:
+        json.dump(existing_data, f, indent=4)
+
